ca33af6ac03c57ca7b8b62d8f0b14ce7a89f1ce5
Alex Sterling <sterlingalex@gmail.com>
2015-02-17 10:46:16 -0500

    Update after.sh

--- src/stubs/after.sh
+++ src/stubs/after.sh
@@ -4,8 +4,9 @@
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
 sudo service nginx stop
-sudo apt-get purge nginx
-sudo apt-get update
-sudo apt-get install apache2
+sudo apt-get purge -y nginx
+sudo apt-get update -y
+sudo apt-get install -y apache2
+sudo apt-get install php5 libapache2-mod-php5
 sudo sed -i -e 's/var\/www\/html/var\/www/g' /etc/apache2/sites-enabled/000-default.conf
 sudo service apache2 restart

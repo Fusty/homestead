588ae3f3aa7321df232a8fbae82faf747f4a8689
Alex Sterling <sterlingalex@gmail.com>
2015-02-17 13:28:49 -0500

    Update after.sh

--- src/stubs/after.sh
+++ src/stubs/after.sh
@@ -6,7 +6,7 @@
 sudo service nginx stop
 sudo apt-get purge --yes --force-yes nginx
 sudo apt-get update --yes --force-yes
-#sudo apt-get install --yes --force-yes libapache2-mod-php5
+sudo apt-get install --yes --force-yes apache2
 sudo sed -i -e 's/var\/www\/html/var\/www/g' /etc/apache2/sites-enabled/000-default.conf
 sudo sed -i -e 's/#application\/x-httpd-p/application\/x-httpd-p/g' /etc/mine.types
 sudo service apache2 restart
